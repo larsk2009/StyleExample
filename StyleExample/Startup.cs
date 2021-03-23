b1e5499a8c85997666353d8293e81280189158e2
Lars Koopmans <larsk2009@live.nl>
2021-03-23 16:01:36 +0100

    Add changes for using a custom class

--- StyleExample/Startup.cs
+++ StyleExample/Startup.cs
@@ -1,3 +1,4 @@
+using Blazored.Modal;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -29,6 +30,8 @@ namespace StyleExample
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
+            services.AddBlazoredModal();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
